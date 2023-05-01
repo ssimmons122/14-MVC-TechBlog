fdcc6488e80b9093b18e47c2366a7a89de96572d
ssimmons122 <121777930+ssimmons122@users.noreply.github.com>
2023-05-01 18:29:51 -0400

    handlebars cards

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,23 @@
 const Sequelize = require('sequelize');
 
 require('dotenv').config();
+
+//connect to db, pass in mysql info
+let sequelize; 
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.EB_PASSWORD, 
+        {
+            host:'localhost',
+            dialect: 'mysql',
+            port: 3306
+        }
+    );
+}
+
+module.exports = sequelize ;
---
+++ server.js
@@ -0,0 +1,16 @@
+
+const express = require("express");
+const exphbs = require("express-handlebars");
+const allRoutes = require("./controllers");
+const session = require("express-session");
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// models
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
